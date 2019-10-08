233153540eaa1d01813399e27c9c1793ac41a564
Brandon Gautama <brandongautama@yahoo.co.id>
2019-10-08 13:58:28 -0700

    Updated threadExample to include sleep

--- threadExample.cpp
+++ threadExample.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <thread>
+#include <chrono>
 
-int function(){
-    std::cout << "I'll run a thing!" << std::endl;
+void functionSleep(){
+    std::cout << "Start functionSleep" << std::endl;
+    std::cout << "Sleeping for 5 seconds...." << std::endl;
+    std::this_thread::sleep_for (std::chrono::seconds(5));
+    std::cout << "Finished sleeping" << std::endl;
 }
 
-int functionWithArguments(int socket, std::string word ){
+void functionWithArguments(int socket, std::string word ){
+    std::cout << std:: endl << "Start functionWithArgs" << socket << std::endl;
     std::cout << "I'll run on socket " << socket << std::endl;
     std::cout << "I'll also read " << word << std::endl;
+    std::cout << "Finished functionWithArgs " << std::endl << std::endl;
 }
 
 int main(){
-    std::thread functionThread (function);
+    std::thread functionThread (functionSleep);
     std::thread argThread (functionWithArguments, 8000, "filename");
     functionThread.join();
     argThread.join();
+    std::cout << "Finished main function" << std::endl;
 }
+
