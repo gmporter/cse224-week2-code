91a82149210caa807bdcdc3bed21ee0452d0483e
Brandon Gautama <brandongautama@yahoo.co.id>
2019-10-08 16:01:07 -0700

    Updated pathExample

--- pathExample.cpp
+++ pathExample.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
-#include <filesystem>
 #include <fstream>
+#include <string>
 int main()
 {
+    std::string path = "location.txt";
 	std::ofstream newfile;
-	newfile.open("location.txt");
-	newfile << "This is a dummy file";
+	newfile.open(path);
+	newfile << "This is a dummy file. \n";
 	newfile.close();
-
-	std::filesystem::path relative = "location.txt";
-	std::filesystem::path absolute = std::filesystem::absolute(relative);
-	std::cout << "Absolute path for " << relative << " is " << absolute << '\n';
 
 	std::ifstream fileReader;
 	std::string line;
 
-	fileReader.open(relative);
-	while (!fileReader.eof()) {
-		fileReader >> line;
-		std::cout << line;
+	fileReader.open(path);
+	while (getline(fileReader, line)) {
+		std::cout << line << std::endl;
 	}
 	fileReader.close();
-
-
-	fileReader.open(absolute);
-	while (!fileReader.eof()) {
-		fileReader >> line;
-		std::cout << line;
-	}
-	fileReader.close();
-
 }
-
